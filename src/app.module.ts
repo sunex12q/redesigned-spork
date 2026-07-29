430401b0574989f2225603fd66402e889ef95737
Akingbonmire sunday <zaboski12@gmail.com>
2026-07-29 05:02:55 +0100

    Add Users resource with Task-User relationship

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { TasksModule } from './tasks/tasks.module';
       }),
     }),
     TasksModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
 
 @Entity()
 export class Task {
@@ -10,4 +11,7 @@ export class Task {
 
   @Column({ default: false })
   completed: boolean;
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  user: User;
 }
